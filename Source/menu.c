702d5348cd001d06033193f25ec085a96e057d56
KokiKitsune2007 <128624869+KokiKitsune2007@users.noreply.github.com>
2023-11-19 21:40:28 -0800

    Update menu.c

--- Source/menu.c
+++ Source/menu.c
@@ -1,3 +1,4 @@
+//main code for interface
 #include <wiiuse/wpad.h>
 #include <wiikeyboard/keyboard.h>
 
@@ -9,5 +10,30 @@
 static void *xfb = NULL;
 static GXRModeObj *rmode = NULL;
 
-int main(int argc, int index, char *Languages char **argv) {
+clrscr = 0;
+
+void Initialise() {
   
+	VIDEO_Init();
+	WPAD_Init();
+	PAD_Init();
+ 
+	rmode = VIDEO_GetPreferredMode(NULL);
+
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+ 
+	VIDEO_Configure(rmode);
+	VIDEO_SetNextFramebuffer(xfb);
+	VIDEO_SetBlack(FALSE);
+	VIDEO_Flush();
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+}
+int main(int argc, int index, char *Languages char **argv) {
+ printf("------------------------------------------------------\n");
+ printf("AnyLanguage Patcher v1.0 by Koki                      \n");
+ printf("------------------------------------------------------\n");
+ printf("Current language: PLaCEHOLDER\n");
+ printf("Selected language: %c\n");
+ printf("-------------------------------
